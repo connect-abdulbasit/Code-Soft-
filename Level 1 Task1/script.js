50f4838a61b9a0d0681acb463c96adbdf7d9645d
connect.abdulbasit <contact.abdulbasit.cs@gmail.com>
2024-05-11 12:06:48 +0500

    task1 completely done

--- Level 1 Task1/script.js
+++ Level 1 Task1/script.js
@@ -1,9 +1,11 @@
 const click=document.querySelector(".icon");
 const options=document.querySelector("#options");
 click.addEventListener("click",()=>{
-    if (options.className==="options") {
-        options.className+=" show";
+    if (options.style.top=="-100%") {
+        options.style.top="0%";
+        options.style.display="block";
     }else{
-        options.classList="options";
+        options.style.top="-100%";
+        options.style.display="none";
     }
 })
